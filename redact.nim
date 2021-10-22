e8c64dafbc3afc1cf913aeb8eb954655c7aa6010
quantimnot <quantimnot@users.noreply.github.com>
2021-10-22 19:03:09 -0400

    rename: sanitize -> redact

--- redact.nim
+++ redact.nim
@@ -8,7 +8,7 @@ type Subs = object
   pegLitSubs: seq[(Peg, string)]
   pegShSubs: seq[(Peg, string)]
 
-type Sanitizer {.sparse.} = object
+typeRedactor{.sparse.} = object
   matchKind: string
   match: string
   replaceKind: string
@@ -24,29 +24,29 @@ proc sh(cmd: string, match = "", len = 0): string =
   result = string shResult.output
   result.stripLineEnd
 
-proc add(subs: var Subs, sanitizer: sink Sanitizer) =
-  case sanitizer.matchKind:
+proc add(subs: var Subs,redactor sinkRedactor =
+  caseredactormatchKind:
   of "sh":
-    let match = sh sanitizer.match
-    case sanitizer.replaceKind:
-    of "lit": add subs.litLitSubs, (match, sanitizer.replace)
-    of "sh": add subs.litLitSubs, (match, sh(sanitizer.replace, match, match.len))
+    let match = shredactormatch
+    caseredactorreplaceKind:
+    of "lit": add subs.litLitSubs, (match,redactorreplace)
+    of "sh": add subs.litLitSubs, (match, shredactorreplace, match, match.len))
     else: discard
   of "lit":
-    case sanitizer.replaceKind:
-    of "lit": add subs.litLitSubs, (sanitizer.match, sanitizer.replace)
-    of "sh": add subs.litLitSubs, (sanitizer.match, sh(sanitizer.replace,
-        sanitizer.match, sanitizer.match.len))
+    caseredactorreplaceKind:
+    of "lit": add subs.litLitSubs, redactormatch,redactorreplace)
+    of "sh": add subs.litLitSubs, redactormatch, shredactorreplace,
+       redactormatch,redactormatch.len))
     else: discard
   of "re":
-    case sanitizer.replaceKind:
-    of "lit": add subs.reLitSubs, (re sanitizer.match, sanitizer.replace)
-    of "sh": add subs.reShSubs, (re sanitizer.match, sanitizer.replace)
+    caseredactorreplaceKind:
+    of "lit": add subs.reLitSubs, (reredactormatch,redactorreplace)
+    of "sh": add subs.reShSubs, (reredactormatch,redactorreplace)
     else: discard
   of "peg":
-    case sanitizer.replaceKind:
-    of "lit": add subs.pegLitSubs, (peg sanitizer.match, sanitizer.replace)
-    of "sh": add subs.pegShSubs, (peg sanitizer.match, sanitizer.replace)
+    caseredactorreplaceKind:
+    of "lit": add subs.pegLitSubs, (pegredactormatch,redactorreplace)
+    of "sh": add subs.pegShSubs, (pegredactormatch,redactorreplace)
     else: discard
   else: discard
 
@@ -95,11 +95,11 @@ for kind, key, value in getOpt():
     discard
 
 if fileExists ruleFilePath:
-  var sanitizers: seq[Sanitizer]
+  varredactors seqRedactor
   var s = newFileStream(ruleFilePath)
-  load(s, sanitizers)
+  load(s,redactors
   s.close()
-  for sanitizer in sanitizers: add subs, sanitizer
+  forredactorinredactors add subs,redactor
 
 if inputs.len > 0:
   for path in inputs:
